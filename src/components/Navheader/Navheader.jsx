79f4a6cce8381f534028f1b68bd477a9c9ccc5fb
Didier-Soldini <soldinididier@wanadoo.fr>
2023-06-23 15:32:59 +0200

    ajout classe active liens header et pointer sur collapse

--- src/components/Navheader/Navheader.jsx
+++ src/components/Navheader/Navheader.jsx
@@ -3,10 +3,28 @@ import { NavLink } from 'react-router-dom'
 const Navheader = () => {
   return (
     <nav className="navheader">
-      <NavLink to="/" className="navheader__link--Accueil">
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? 'navheader__link--Accueil'
+            : isActive
+            ? 'navheader__link--Accueil--active'
+            : 'navheader__link--Apropos'
+        }
+      >
         Accueil
       </NavLink>
-      <NavLink to="/about" className="navheader__link--Apropos">
+      <NavLink
+        to="/about"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? 'navheader__link--Apropos'
+            : isActive
+            ? 'navheader__link--Apropos--active'
+            : 'navheader__link--Accueil'
+        }
+      >
         A Propos
       </NavLink>
     </nav>
